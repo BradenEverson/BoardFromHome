f8c91f34766fcf02536e9aaf1616fedec70181f2
Braden Everson <bradeneverson@gmail.com>
2020-08-07 12:54:18 -0500

    Added game class(semi abstract) empty database library and create, join and game pages

---
+++ BoardFromHome.Core/BoardGame.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoardFromHome.Core
+{
+    public enum gameType
+    {
+        board,
+        card,
+        dice
+    }
+    public class BoardGame
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int minPlayers { get; set; }
+        public int maxPlayers { get; set; }
+        public gameType gameType { get; set; }
+        public Dictionary<string,int> uniqueItemAmounts { get; set; }
+        public string serializedUniqueItemAmounts { get; set; }//Format will be string:int,
+        public string[] ruleSet { get; set; }
+        public string serializedRuleSet { get; set; }//Format will be rule + ,
+        public void serializeRuleSet()
+        {
+            this.serializedRuleSet = "";
+            foreach(string rule in ruleSet)
+            {
+                serializedRuleSet += rule + ",";
+            }
+        }
+        public void serializeUniqueItemAmounts()
+        {
+            this.serializedUniqueItemAmounts = "";
+            foreach(var valuePair in uniqueItemAmounts.Keys)
+            {
+                this.serializedUniqueItemAmounts += valuePair + ":" + uniqueItemAmounts[valuePair] + ",";
+            }
+        }
+        public void deSerializeRules()
+        {
+            this.ruleSet = this.serializedRuleSet.Split(',');
+        }
+        public void deSerializeUniqueItems()
+        {
+            this.uniqueItemAmounts = new Dictionary<string, int>();
+            foreach(string keyValPair in this.serializedUniqueItemAmounts.Split(','))
+            {
+                this.uniqueItemAmounts.Add(keyValPair.Split(':')[0], int.Parse(keyValPair.Split(':')[1]));
+            }
+        }
+    }
+    
+}
